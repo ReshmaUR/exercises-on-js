073d41fc2a52f2957ca31e35dcfd437a1f1617c6
ReshmaUR <reshsaradhi010@gmail.com>
2020-05-10 10:36:00 +0530

    adding html js and css files

---
+++ ans.js
@@ -0,0 +1,169 @@
+//Question1
+function primeorNot(){
+    var number = [];
+    var size = document.getElementById("pri").value; //max array size
+    for(var i =0;i<size;i++){
+        number[i] = prompt("Enter element"+(i+1));
+    }
+    var x = number[0];
+    document.getElementById("array").value = number;
+    if(x===1){
+        document.getElementById("cmt").innerHTML = "The first element is not a prime number";
+        return false;
+       
+    }
+    else if(x===2){ 
+        document.getElementById("cmt").innerHTML = "The first element is a prime number";
+        return true;
+        
+    }
+    else{
+        for(var i=2;i<x;i++){
+            if(x%i ===0){
+                document.getElementById("cmt").innerHTML = "The first element is not a prime number";
+                return false;
+                
+            }
+        }
+    }
+    document.getElementById("cmt").innerHTML = "The first element is a prime number";
+    return true;   
+}
+//Question2
+function frequentItem(){
+var array=[];
+var mf = 1;
+var m = 0;
+var item;
+var size = document.getElementById("size").value;
+for(var i =0;i<size;i++){
+    array[i] = prompt("Enter element"+(i+1));
+}
+document.getElementById("freq").value = array;
+for (var i=0; i<array.length; i++){
+    for (var j=i; j<array.length; j++){
+                if (array[i] == array[j])
+                 m++;
+                if (mf<m)
+                {
+                  mf=m; 
+                  item = array[i];
+                }
+        }
+        m=0;
+}
+console.log(item+" ( " +mf +" times ) ") ;
+document.getElementById("item").value = item;
+}
+//Question3
+function swap(){
+    var str = document.getElementById("input").value;
+    var UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var LOWER = 'abcdefghijklmnopqrstuvwxyz';
+    var result = [];
+    for(var x=0; x<str.length; x++){
+
+        if(UPPER.indexOf(str[x]) !== -1){
+             result.push(str[x].toLowerCase());
+        }
+        else if(LOWER.indexOf(str[x]) !== -1){
+            result.push(str[x].toUpperCase());
+        }
+        else {
+            result.push(str[x]);
+        }
+    }
+    console.log(result.join(''));
+    document.getElementById("swap").value = result.join('');
+}
+//Question4
+function square(){
+    var array = [];
+    var size = document.getElementById("sno").value;
+    for(var i =0;i<size;i++){
+    array[i] = prompt("Enter element"+(i+1));
+    }
+    document.getElementById("snoo").value = array;
+    var sum = 0; 
+    i = array.length;
+    while (i--) 
+    sum += Math.pow(array[i], 2);
+    console.log(sum); 
+    document.getElementById("square").value = sum;
+}
+//Question5
+function oddorEven(){
+    var num = "";
+    for(var i = 0;i<16;i++){
+        if(i%2 == 0){
+            num += "Iteration ="+i+" , It is even "+"<br>";
+        }
+        else{
+            num += "Iteration ="+i+" , It is odd"+"<br>";
+        }
+    }
+    document.getElementById("disp").innerHTML = num;
+}
+//Question6
+function trunc(){
+    var str = document.getElementById("stri").value;
+    var limit = document.getElementById("limi").value;
+    var str1;
+    str1 =str.slice(0, limit);
+    console.log(str1);
+    document.getElementById("tru").value = str1;
+}
+    
+    
+
+
+//Question7
+function findLarge(){
+    var num1 = parseInt(document.getElementById("inum1").value);
+    var num2 = parseInt(document.getElementById("inum2").value);   
+    if(num1>num2){
+        document.getElementById("answer7").value = num1;       
+    }
+    else{
+        document.getElementById("answer7").value = num2;
+    }
+}
+//Question8
+function alpha() {
+    var person = []; 
+    var size = document.getElementById("alpha").value; //max array size
+    var x;
+    for(var i =0;i<size;i++){
+        person[i] = prompt("Enter element"+(i+1));
+    }
+    document.getElementById("old").value = person;
+    for (x in person) {
+    person.sort();
+    }
+    document.getElementById("new").value = person;
+  }
+  
+//Question9
+function printLtr(){
+var txt = "JavaScript";
+var x;
+var out ="";
+var final ="";
+for (x of txt) {
+out += x;
+final += out+"<br>";
+}
+document.getElementById("toge").innerHTML = final;
+}
+//Question10
+function pattern(){
+    var limit = 3;
+    var star = "";
+    for(var i = 0;i<=limit;i++){
+        for(var j = 0;j<=i;j++){
+            star += "*";
+        }
+            star += "<br>";
+    }
+    document.getElementById("pat").innerHTML = star;
+}
